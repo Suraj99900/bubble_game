11483e99bd9443fbcd6e1ce546cc65069e6aea38
Suraj_SJ <jaiswaljesus384@gmail.com>
2022-12-31 22:15:26 +0530

    move functionality

--- lib/button.dart
+++ lib/button.dart
@@ -1,21 +1,26 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables, prefer_typing_uninitialized_variables
 
 import 'dart:html';
 
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  final icon;
+  final function;
+  const MyButton({super.key, this.icon, this.function});
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: Container(
-        width: 50,
-        height: 50,
-        color: Colors.grey[100],
-        child: Center(child: Icon(Icons.arrow_back)),
+    return GestureDetector(
+      onTap: function,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          width: 50,
+          height: 50,
+          color: Colors.grey[100],
+          child: Center(child: Icon((icon))),
+        ),
       ),
     );
   }
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -9,6 +9,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double playerX = 0;
+
+  void moveLeft() {
+    setState(() {
+      playerX -= 0.2;
+    });
+  }
+
+  void moveRight() {
+    setState(() {
+      playerX += 0.2;
+    });
+  }
+
+  void moveFire() {}
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -17,6 +33,23 @@ class _HomePageState extends State<HomePage> {
           flex: 3,
           child: Container(
             color: Colors.pink[100],
+            child: Center(
+              child: Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Container(
+                      alignment: Alignment(playerX, 1),
+                      child: Container(
+                        color: Colors.purple,
+                        height: 50,
+                        width: 50,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
         Expanded(
@@ -24,11 +57,19 @@ class _HomePageState extends State<HomePage> {
           color: Colors.grey,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // ignore: prefer_const_constructors
-            children: const [
-              MyButton(),
-              MyButton(),
-              MyButton(),
+            children: [
+              MyButton(
+                icon: Icons.arrow_back,
+                function: moveLeft,
+              ),
+              MyButton(
+                icon: Icons.arrow_upward,
+                function: moveFire,
+              ),
+              MyButton(
+                icon: Icons.arrow_forward,
+                function: moveRight,
+              ),
             ],
           ),
         ))
